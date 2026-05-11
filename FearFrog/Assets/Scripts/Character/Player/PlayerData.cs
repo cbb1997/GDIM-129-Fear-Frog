a0a06d536966cfc0fe6b1abea8a52816ec5a02d3
MOBIUS <yuchenliu0710@outlook.com>
2026-05-11 00:40:52 -0700

    add player data into PlayerData scriptable object class

--- FearFrog/Assets/Scripts/Character/Player/PlayerData.cs
+++ FearFrog/Assets/Scripts/Character/Player/PlayerData.cs
@@ -2,7 +2,24 @@ using UnityEngine;
 using System;
 
 [System.Serializable]
-public class PlayerDataClass : CharacterDataClass { }
+public class PlayerDataClass : CharacterDataClass
+{
+    // Player movement
+    [SerializeField] private float m_walkAcceleration = 30f;
+    public float WalkAcceleration { get { return m_walkAcceleration; } }
+    [SerializeField] private float m_sprintAcceleration = 45f;
+    public float SprintAcceleration { get { return m_sprintAcceleration; } }
+    [SerializeField] private float m_crouchAcceleration = 18f;
+    public float CrouchAcceleration { get { return m_crouchAcceleration; } }
+    [SerializeField] private float m_cameraSensitivity = 100f;
+    public float CameraSensitivity { get { return m_cameraSensitivity; } }
+    [SerializeField] private float m_jumpAcceleration = 320f;
+    public float JumpAcceleration { get { return m_jumpAcceleration; } }
+    
+    // Player camera bob
+    [SerializeField] private bool m_bobEnabled = true;
+    public bool BobEnabled { get { return m_bobEnabled; } }
+}
 
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/PlayerData")]
