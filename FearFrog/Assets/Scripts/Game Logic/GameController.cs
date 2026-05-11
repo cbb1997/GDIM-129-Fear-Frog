a429962a94c9da7051682822e1b653cb88a00e79
cbb1997 <cbd45068@gmail.com>
2026-05-10 20:33:50 -0700

    Setup first prototype scene

--- FearFrog/Assets/Scripts/Game Logic/GameController.cs
+++ FearFrog/Assets/Scripts/Game Logic/GameController.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private VisualDictionary<string, int> m_Dict;
 
     private void Start()
     {
         Debugger.Log("Engine initialized...");
 
-        m_Dict = new VisualDictionary<string, int>(5);
     }
 }
