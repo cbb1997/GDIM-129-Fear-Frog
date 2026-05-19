4b9b6368061976b32e86aaf61176c7b069aafe3f
lsabe37 <lsabe@uci.edu>
2026-05-19 16:42:29 -0700

    ui stuff

--- FearFrog/Assets/Scripts/UI/TestTest.cs
+++ FearFrog/Assets/Scripts/UI/TestTest.cs
@@ -5,12 +5,9 @@ public class TestTest : MonoBehaviour
     public InventoryController inventoryController;
     public ItemData items;
 
-    void Update()
+    private void OnMouseDown()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            PickUpItem();
-        }
+        PickUpItem();
     }
 
     public void PickUpItem()
