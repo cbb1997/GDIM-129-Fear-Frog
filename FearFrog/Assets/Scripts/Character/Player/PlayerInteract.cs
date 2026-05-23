64fb82aa8eccf92a36973089b5e0c4adcbb08614
MOBIUS <yuchenliu0710@outlook.com>
2026-05-23 11:40:31 -0700

    fix player interact to be performed using [F]

--- FearFrog/Assets/Scripts/Character/Player/PlayerInteract.cs
+++ FearFrog/Assets/Scripts/Character/Player/PlayerInteract.cs
@@ -1,32 +1,46 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInteract : MonoBehaviour
 {
     // Member variables
-    [SerializeField] private float m_maxDistance = 1.2f;
+    [SerializeField] private float m_maxDistance = 1.7f;
     private Vector3 m_screenCenter;
+    private IInteractable interactableObj;
     
     
     // Start
     private void Start()
     {
         m_screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+        
+        // Link player interaction functionality
+        InputController.Instance.Input.Player.Interact.performed += Interact;
     }
-
+    
     // Update
     private void Update()
     {
         // Check for interactable objects
         Ray ray = PlayerController.Instance.Camera.GetComponent<Camera>().ScreenPointToRay(m_screenCenter);
-        if (Physics.Raycast(ray, out RaycastHit hit, m_maxDistance))
+        interactableObj = (Physics.Raycast(ray, out RaycastHit hit, m_maxDistance)) 
+            ? hit.collider.GetComponent<IInteractable>() : null;
+        Debug.DrawLine(ray.origin, ray.origin + m_maxDistance * ray.direction);
+        // Update UI prompt
+        // if (interactableObj != null)
+        // {
+        //     SomeEvent?.Invoke();
+        // }  
+    }
+
+    // Perform player interaction
+    public void Interact(InputAction.CallbackContext ctx)
+    {
+        if (interactableObj != null && interactableObj.CanInteract)
         {
-            IInteractable interactableObj = hit.collider.GetComponent<IInteractable>();
-            if (interactableObj != null)
-            {
-                interactableObj.OnInteract();
-            }   
-        }
+            interactableObj.OnInteract();
+        }   
     }
 }
