e632c6250fa14ed57eece099201670b7826400a9
MOBIUS <yuchenliu0710@outlook.com>
2026-05-23 09:50:07 -0700

    add player interact functionality and finish testing

---
+++ FearFrog/Assets/Scripts/Character/Player/PlayerInteract.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    // Member variables
+    [SerializeField] private float m_maxDistance = 1.2f;
+    private Vector3 m_screenCenter;
+    
+    
+    // Start
+    private void Start()
+    {
+        m_screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+    }
+
+    // Update
+    private void Update()
+    {
+        // Check for interactable objects
+        Ray ray = PlayerStatus.Instance.Camera.GetComponent<Camera>().ScreenPointToRay(m_screenCenter);
+        if (Physics.Raycast(ray, out RaycastHit hit, m_maxDistance))
+        {
+            IInteractable interactableObj = hit.collider.GetComponent<IInteractable>();
+            if (interactableObj != null)
+            {
+                interactableObj.OnInteract();
+            }   
+        }
+    }
+}
--- FearFrog/Assets/Scripts/Interactable/InteractableCube.cs
+++ FearFrog/Assets/Scripts/Interactable/InteractableCube.cs
@@ -6,15 +6,9 @@ public class InteractableCube : MonoBehaviour, IInteractable
     // Interactable interface member variables
     private bool m_canInteract = true;
     public bool CanInteract { get { return m_canInteract; } set { m_canInteract = value; } }
-    private string m_promptMessage = "";
+    [SerializeField] private string m_promptMessage = "";
     public string PromptMessage { get { return m_promptMessage; } }
     
-    
-    // Start
-    private void Start()
-    {
-        m_promptMessage = "Test Cube Being Interacted";
-    }
 
     // OnInteract() override
     public void OnInteract()
