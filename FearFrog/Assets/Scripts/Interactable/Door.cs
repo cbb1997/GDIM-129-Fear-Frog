b31248262e004f1736def33ffe580cff13540170
lsabe37 <lsabe@uci.edu>
2026-06-01 23:39:37 -0700

    notification ui setup

--- FearFrog/Assets/Scripts/Interactable/Door.cs
+++ FearFrog/Assets/Scripts/Interactable/Door.cs
@@ -4,6 +4,7 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private int DoorID;
     [SerializeField] private InventoryData inventoryData;
+    [SerializeField] private string RequiredKeyName;
 
     [Header ("TestStuff")]
     [SerializeField] private float pushForce = 10f;
@@ -19,10 +20,12 @@ public class Door : MonoBehaviour
         if (inventoryData.CheckItems(DoorID))
         {
             rb.AddForce(transform.forward * pushForce, ForceMode.Impulse);
+            UILocator.Instance.notifUI.HideNotif();
+
         }
         else
         {
-            Debug.Log("Find Key!");
+            UILocator.Instance.notifUI.ShowNotif(RequiredKeyName);
         }
 
     }
