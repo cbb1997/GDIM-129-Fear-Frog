71d1753cedf7f9c2305177b6812afe7a84776fdc
cbb1997 <cbd45068@gmail.com>
2026-06-02 00:10:28 -0700

    Door script

--- FearFrog/Assets/Scripts/Interactable/LockedDoor.cs
+++ FearFrog/Assets/Scripts/Interactable/LockedDoor.cs
@@ -4,7 +4,33 @@ public class LockedDoor : MonoBehaviour
 {
     [SerializeField] private int m_DoorID;
     [SerializeField] private InventoryData m_InventoryData;
-    [SerializeField] private string m_RequiredKeyName;
+    [SerializeField] private string m_KeyName;
+    [SerializeField] private Animator m_Animator;
+    
+    private DoorState m_CurrentState;
+    private bool m_Locked;
+
+    private void OnMouseDown()
+    {
+        if (m_Locked)
+        {
+
+        }
+
+        SwitchState();
+    }
+
+    private void SwitchState()
+    {
+        if (m_CurrentState == DoorState.Closed) SetState(DoorState.Open);
+        else if (m_CurrentState == DoorState.Open) SetState(DoorState.Closed);
+    }
+
+    private void SetState(DoorState state)
+    {
+        m_CurrentState = state;
+        m_Animator.SetInteger("AnimState", (int)m_CurrentState);
+    }
 
     /*
     private void OnMouseDown()
@@ -17,7 +43,6 @@ public class LockedDoor : MonoBehaviour
         {
             UILocator.Instance.notifUI.ShowNotif(RequiredKeyName);
         }
-
     }
     */
 }
