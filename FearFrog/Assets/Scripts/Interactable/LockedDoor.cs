952ed4fb1be1ec8b3fcba5afde2fb6f0c8a1f73f
cbb1997 <cbd45068@gmail.com>
2026-06-05 00:18:22 -0700

    Locked door

--- FearFrog/Assets/Scripts/Interactable/LockedDoor.cs
+++ FearFrog/Assets/Scripts/Interactable/LockedDoor.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
+using System;
 
 public class LockedDoor : MonoBehaviour
 {
-    [SerializeField] private int m_DoorID;
     [SerializeField] private InventoryData m_InventoryData;
-    [SerializeField] private string m_KeyName;
+    [SerializeField] private int m_KeyID;
     [SerializeField] private Animator m_Animator;
+
+    public static Action OnFailedUnlock;
     
     private DoorState m_CurrentState;
-    private bool m_Locked;
+    private bool m_Locked = true;
 
     private void OnMouseDown()
     {
-        if (m_Locked)
+        if (m_Locked && !m_InventoryData.CheckItems(m_KeyID))
         {
-
+            OnFailedUnlock?.Invoke();
+            return;
         }
 
         SwitchState();
